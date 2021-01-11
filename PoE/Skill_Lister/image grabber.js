34999d3ee2ed446360b512cecc0b48f9120c7104
AQtun <Lysy9990@Gmail.com>
2021-01-11 15:31:31 +0100

    updated icons

--- PoE/Skill_Lister/image grabber.js
+++ PoE/Skill_Lister/image grabber.js
@@ -1,6 +1,9 @@
 // made for path of exile skill lister
 // use at https://pathofexile.gamepedia.com/Skill_gem
 // copy console output
+// download icons with
+// firefox: https://addons.mozilla.org/en-US/firefox/addon/turbo-download-manager
+// chrome: https://chrome.google.com/webstore/detail/tab-save/lkngoeaeclaebmpkgapchgjdbaekacki
 
 result = '';
 
@@ -13,6 +16,6 @@ for (var e of elements) {
   }
   imgurl = img[0].children[0].src;
   console.log(imgurl);
-  result += imgurl + '/replace this with line break/';
+  result += imgurl + '\n';
 }
 console.log(result);
