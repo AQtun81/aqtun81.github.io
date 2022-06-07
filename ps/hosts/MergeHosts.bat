be78f7072b678d7b67b23444befa1df32fb2d716
unknown <lysy9990@gmail.com>
2022-06-07 19:52:22 +0200

    improve remote piping batch

--- ps/hosts/MergeHosts.bat
+++ ps/hosts/MergeHosts.bat
@@ -2,6 +2,6 @@
 cd "%~dp0"
 %~d0
 cls
-powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/AQtun81/aqtun81.github.io/master/ps/hosts/MergeHosts.ps1' | Invoke-Expression}"
+powershell -executionpolicy remotesigned -Command "(Invoke-WebRequest 'https://raw.githubusercontent.com/AQtun81/aqtun81.github.io/master/ps/hosts/MergeHosts.ps1').Content | Invoke-Expression"
 pause
 @echo on
