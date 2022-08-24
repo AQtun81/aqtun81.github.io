6fa9ba45b512e9e0e5b3fd41266d474ba2fafe3c
unknown <lysy9990@gmail.com>
2022-08-24 16:17:27 +0200

    Moving to separate repository

--- ps/hosts/MergeHosts.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd "%~dp0"
-%~d0
-cls
-powershell -executionpolicy remotesigned -Command "(Invoke-WebRequest 'https://raw.githubusercontent.com/AQtun81/aqtun81.github.io/master/ps/hosts/MergeHosts.ps1').Content | Invoke-Expression"
-pause
-@echo on
