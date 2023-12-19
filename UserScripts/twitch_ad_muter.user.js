76fae45c6d0b7ae6432cc8c64ba3f6db9d27525d
AQtun81 <Lysy9990@Gmail.com>
2023-12-19 07:37:00 +0100

    Update twitch_ad_muter.user.js

--- UserScripts/twitch_ad_muter.user.js
+++ UserScripts/twitch_ad_muter.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Twitch Automatic ADs muter
 // @namespace    https://www.twitch.tv
-// @version      1.0
+// @version      1.1
 // @description  Read Title :)
 // @author       AQtun81
 
+// @history      1.1 updated
 // @history      1.0 initial release
 
 // @include		http://twitch.tv/*
@@ -16,28 +17,35 @@
 // @exclude		https://api.twitch.tv/*
 
 // @icon         https://www.google.com/s2/favicons?domain=twitch.tv
-
 // @grant        none
 
 // ==/UserScript==
 
 window.muteAD = function() {
-  element = document.querySelector("div.tw-c-background-overlay:nth-child(4)");
-  if (element != null && !isMuted) {
-      userVolume = document.querySelector(".video-player__container > video").volume; // store current volume
-      isMuted = true;
-      document.querySelector(".video-player__container > video").volume = 0; // mute stream
-      //console.log("AD is playing! attempting to mute");
-  } else if (element == null && isMuted) {
-      document.querySelector(".video-player__container > video").volume = userVolume; // resume volume
-      isMuted = false;
-      //console.log("AD ended! resuming volume");
-  }
+    var element = document.querySelector('[data-a-target="video-ad-label"]');
+
+    // ADs started
+    if (element != null && !isMuted) {
+        var videoElement = document.querySelector(".video-player__container video");
+        if (videoElement.muted) { return; } // stream is already muted, this prevents unmuting when ads end
+        console.log('%c[ADs Muter]' + '%c Muting Player', 'background: #222; color: #32a869', '');
+        isMuted = true;
+        videoElement.muted = true;
+        return;
+    }
+
+    // ADs ended
+    if (element == null && isMuted) {
+        var videoElement = document.querySelector(".video-player__container video");
+        console.log('%c[ADs Muter]' + '%c Resuming Player Volume', 'background: #222; color: #32a869', '');
+        videoElement.muted = false;
+        isMuted = false;
+        return;
+    }
 };
 
 (function() {
     'use strict';
-    window.userVolume = 0.1;
     window.isMuted = false;
-    setInterval(muteAD, 100);
+    setInterval(muteAD, 16);
 })();
